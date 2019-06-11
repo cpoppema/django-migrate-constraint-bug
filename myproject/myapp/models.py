61d8cb96752ec75c9fa262745f25aed79867e54a
cpoppema <cpoppema@gmail.com>
2019-06-11 09:56:42 +0200

    Cannot add foreign key constraint.

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
-    code = models.PositiveIntegerField(unique=True)
+    code = models.CharField(unique=True, max_length=15)
 
 
 class Call(models.Model):
     client = models.ForeignKey(Client,
                                on_delete=models.CASCADE,
                                to_field='code',
-                               related_name='+',
                                db_column='clientcode')
